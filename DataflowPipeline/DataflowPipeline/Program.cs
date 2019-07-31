751d50740af0e9df36768c566188e089d03b75a0
Peter Lee <peter.lee@armeta.com>
2019-07-31 11:23:48 -0500

    Resolved the completion of Output pipelines

--- DataflowPipeline/DataflowPipeline/Program.cs
+++ DataflowPipeline/DataflowPipeline/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -72,12 +73,20 @@ namespace DataflowPipeline
             // Creates actions for the appropriate buffers
             var printOddWords = new ActionBlock<string>(word =>
             {
-                Console.WriteLine("Odd word: {0} {1}", word, word.Length);
+                using (StreamWriter file =
+                    new StreamWriter(@"C:\Users\Peter.Lee\Desktop\temp\oddwords.txt", true))
+                {
+                    file.WriteLine(word);
+                }
             });
 
             var printEvenWords = new ActionBlock<string>(word =>
             {
-                Console.WriteLine("Even word: {0} {1}", word, word.Length);
+                using (StreamWriter file =
+                    new StreamWriter(@"C:\Users\Peter.Lee\Desktop\temp\evenwords.txt", true))
+                {
+                    file.WriteLine(word);
+                }
             });
 
             //
@@ -86,24 +95,35 @@ namespace DataflowPipeline
 
             var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
 
+            // Linking the first pipe line.
             downloadString.LinkTo(createWordList, linkOptions);
             createWordList.LinkTo(filterWordList, linkOptions);
             filterWordList.LinkTo(filterOddEven, linkOptions);
 
+            // Creating a completion link between original pipeline and two output pipelines
+            filterOddEven.Completion.ContinueWith(_ => OddBuffer.Complete());
+            filterOddEven.Completion.ContinueWith(_ => EvenBuffer.Complete());
+
+            // Linking buffer blocks to action blocks.
             OddBuffer.LinkTo(printOddWords, linkOptions);
             EvenBuffer.LinkTo(printEvenWords, linkOptions);
 
 
             // Process "The Iliad of Homer" by Homer.
             downloadString.Post("http://www.gutenberg.org/files/6130/6130-0.txt");
+            File.WriteAllText(@"C:\Users\Peter.Lee\Desktop\temp\oddwords.txt", string.Empty);
+            File.WriteAllText(@"C:\Users\Peter.Lee\Desktop\temp\evenwords.txt", string.Empty);
 
             // Mark the head of the pipeline as complete.
             downloadString.Complete();
 
             // Create a task array to wait for all tasks to finish.
-            Task[] pipelineTask = {printOddWords.Completion, printEvenWords.Completion};
+            // Simply writing down Completion.Wait() for all output pipes should suffice?
+            Task[] pipelineTask = { printOddWords.Completion, printEvenWords.Completion };
 
             Task.WaitAll(pipelineTask);
+
+            Console.WriteLine("Done");
         }
     }
 }
